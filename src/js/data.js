dd8aed1ba335189d8741db416149c884e49eb4da
valeria <valeria.penaloza5@gmail.com>
2018-07-12 10:16:30 -0400

    maquetacion crear cuenta e inicio de sesion con facebook

--- src/js/data.js
+++ src/js/data.js
@@ -1,9 +1,24 @@
 window.data = {};
 
 window.data.register = (makeMail, makePassword) => {
-  firebase.auth().createUserWithEmailAndPassword(makeMail, makePassword).catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log('errorCode', errorCode, 'errorMessage', errorMessage);
-  });
+	firebase.auth().createUserWithEmailAndPassword(makeMail, makePassword).catch((error) => {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		console.log('errorCode', errorCode, 'errorMessage', errorMessage);
+	});
 };
+
+function loginFace(){
+	const provider = new firebase.auth.FacebookAuthProvider();
+	provider.setCustomParameters({
+		'display':'popup'
+	});
+	firebase.auth().signInWithPopup(provider)
+	.then(() => {
+		console.log("login con facebook");
+	})
+	.catch((error) => {
+		console.log("error de firebase > "+error.code);
+		console.log("error de firebase, mensaje > "+error.message);
+	});
+}
